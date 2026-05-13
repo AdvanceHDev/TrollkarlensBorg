8c7f9e58589be037cb05151627474dc203e6b289
AdvanceHD <ville.bjorklundstrid@elev.ga.lbs.se>
2026-05-13 10:04:52 +0200

    Börja på nytt för att göra arbetet lättare

---
+++ Trollkarlens_Borg/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Trollkarlens_Borg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
