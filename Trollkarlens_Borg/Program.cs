8f6333f8f4c05f2103357026fe56a10221ddcfce
AdvanceHD <ville.bjorklundstrid@elev.ga.lbs.se>
2026-05-25 00:01:38 +0200

    mer dålig kod

--- Trollkarlens_Borg/Program.cs
+++ Trollkarlens_Borg/Program.cs
@@ -1,10 +1,5 @@
 ﻿namespace Trollkarlens_Borg
 {
-    /* Funktioner att lägga till:
-     * Sparning
-     * Automatisk sparning om man trycker Ctrl + C (Console.CancelKeyPress)
-    */
-
     internal class Program
     {
         static void Main(string[] args)
@@ -12,51 +7,25 @@
             Player? player = null;
             do
             {
-                Console.WriteLine("Välj din hjälte:");
-                Console.WriteLine("1. Eldhjälte");
-                Console.WriteLine("2. Ishjälte");
-                string? input = Console.ReadLine();
-
-                if (int.TryParse(input, out int choice))
-                {
-                    player = Player.ChooseHero(choice);
-                }
+                player = Player.ChooseHero();
             }
             while (player == null);
 
-            while (true)
-            {
-                Console.Write("Välj ett namn för din karaktär: ");
-                string? input = Console.ReadLine()?.Trim();
-
-                if (!string.IsNullOrWhiteSpace(input))
-                {
-                    player.Name = input;
-                    break;
-                }
-            }
+            player.ChooseName();
 
             player.PresentPlayer();
 
-            // Loop starts here?
-            player.FindPossibleDirections();
-
-            while (true)
+            Game.Start();
+            while (Game.IsRunning)
             {
-                Console.WriteLine("\nVart vill du gå?");
-                string? input = Console.ReadLine();
+                Board.ShowMap(player);
 
-                if (!int.TryParse(input, out int choice)) continue;
+                player.FindPossibleDirections();
+                player.Move();
 
-                if (player.PossibleDirections.ContainsKey(choice))
-                {
-                    player.Move(choice);
-                    break;
-                }
+                Room room = Room.ChooseRandom();
+                room.Enter(player);
             }
-
-            Room room = Room.ChooseRandom();
-            room.Enter();
         }
     }
 
@@ -75,42 +44,113 @@
 
     static class Game
     {
+        public static bool IsRunning
+        {
+            get { return _isRunning; }
+        }
 
+        private static bool _isRunning;
+
+        public static void Start()
+        {
+            _isRunning = true;
+        }
+
+        public static void End()
+        {
+            Console.WriteLine("Game over.");
+            _isRunning = false;
+        }
     }
 
     abstract class Board
     {
         public const int Size = 7;
+
+        public static void ShowMap(Player player)
+        {
+            Console.WriteLine();
+
+            for (int i = Size; i > 0; i--)
+            {
+                Console.Write("|");
+
+                for (int j = 1; j <= Size; j++)
+                {
+                    Console.Write(player.Position == (j, i) ? "¤" : " ");
+                    Console.Write("|");
+                }
+                Console.WriteLine();
+            }
+        }
     }
 
     abstract class Room
     {
-        //private string _description;
-
-        //public Room(string description)
-        //{
-
-        //}
-
         public static Room ChooseRandom()
         {
             Random random = new Random();
 
-            return random.Next(2) switch
+            return random.Next(3) switch
             {
                 0 => new TrapRoom(),
-                1 => new EnemyRoom()
+                1 => new EnemyRoom(),
+                2 => new EmptyRoom()
             };
         }
 
-        public abstract void Enter();
+        public abstract void Enter(Player player);
     }
 
     class EmptyRoom : Room
     {
-        public override void Enter()
+        public override void Enter(Player player)
         {
-            
+            Console.WriteLine("\nDetta rummet är tomt.");
+        }
+    }
+
+    class ChestRoom : Room
+    {
+        public override void Enter(Player player)
+        {
+            Console.WriteLine("\nDetta rummet har en kista.");
+            PromptChestOpen(player);
+        }
+
+        private void PromptChestOpen(Player player)
+        {
+            while (true)
+            {
+                Console.WriteLine("\nVill du öppna kistan? 8 av 10 gånger är innehållet ofarligt.");
+                Console.Write("J/n? ");
+                ConsoleKey input = Console.ReadKey().Key;
+
+                if (input == ConsoleKey.J)
+                {
+                    Random random = new Random();
+
+                    switch (random.Next(1, 11))
+                    {
+                        case <= 2:
+                            Console.WriteLine("\nDu hade otur och fick en giftig dryck.");
+                            Console.WriteLine("-5 hälsa");
+                            player.Health -= 5;
+                            break;
+                        case > 2:
+                            Console.WriteLine("\nDu fick en helande dryck.");
+                            Console.WriteLine("+5 hälsa");
+                            player.Health += 5;
+                            break;
+                    }
+
+                    return;
+                }
+                else if (input == ConsoleKey.N)
+                {
+                    return;
+                }
+            }
         }
     }
 
@@ -118,7 +158,7 @@
     {
         private Enemy _enemy;
 
-        public override void Enter()
+        public override void Enter(Player player)
         {
 
         }
@@ -126,7 +166,7 @@
 
     class EnemyRoom : Room
     {
-        public override void Enter()
+        public override void Enter(Player player)
         {
 
         }
@@ -134,34 +174,72 @@
 
     abstract class Player : IAttack, IHealth
     {
-        public string Name { get; set; }
-        public int Health { get; set; }
-
-        public Dictionary<int, string> PossibleDirections
+        public int Health
         {
-            get { return _possibleDirections; }
+            get { return _health; }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine($"\n{_name} har dött.");
+                    Game.End();
+                }
+                else
+                {
+                    _health = value;
+                    Console.WriteLine($"\n{_name}s hälsa är nu {_health}");
+                }
+            }
         }
 
+        public (int, int) Position
+        {
+            get { return _position; }
+        }
+
+        private int _health;
+        private string? _name;
         private (int, int) _position;
         private Dictionary<int, string> _possibleDirections;
 
         public Player(int health)
         {
-            Name = string.Empty;
-            Health = health;
-
+            _health = health;
+            _name = null;
             _position = (1, 1);
             _possibleDirections = new Dictionary<int, string>();
         }
 
-        public static Player? ChooseHero(int choice)
+        public static Player? ChooseHero()
         {
+            Console.WriteLine("Välj din hjälte:");
+            Console.WriteLine("1. Eldhjälte");
+            Console.WriteLine("2. Ishjälte");
+            string? input = Console.ReadLine();
+
+            if (!int.TryParse(input, out int choice)) return null;
+
             return choice switch
             {
                 1 => new FireHero(),
                 2 => new IceHero(),
                 _ => null
             };
+        }
+
+        public void ChooseName()
+        {
+            do
+            {
+                Console.Write("\nVälj ett namn för din karaktär: ");
+                string? input = Console.ReadLine()?.Trim();
+
+                if (!string.IsNullOrWhiteSpace(input))
+                {
+                    _name = input;
+                }
+            }
+            while (_name == null);
         }
 
         public void FindPossibleDirections()
@@ -186,7 +264,7 @@
                 _possibleDirections.Add(_possibleDirections.Count + 1, "Vänster");
             }
 
-            Console.WriteLine($"\n{Name} kan gå:");
+            Console.WriteLine($"\n{_name} kan gå:");
             foreach (int num in _possibleDirections.Keys)
             {
                 string direction = _possibleDirections[num];
@@ -194,24 +272,44 @@
             }
         }
 
-        public void Move(int direction)
+        public void Move()
         {
-            string d = _possibleDirections[direction];
-
-            switch (d)
+            string? direction = null;
+            do
             {
-                case "Upp": _position.Item2++;
+                Console.WriteLine("\nVart vill du gå?");
+                string? input = Console.ReadLine();
+
+                if (int.TryParse(input, out int choice))
+                {
+                    _possibleDirections.TryGetValue(choice, out direction);
+                }
+            }
+            while (direction == null);
+
+            switch (direction)
+            {
+                case "Upp":
+                    _position.Item2++;
                     break;
-                case "Ned": _position.Item2--;
+                case "Ned":
+                    _position.Item2--;
                     break;
-                case "Höger": _position.Item1++;
+                case "Höger":
+                    _position.Item1++;
                     break;
-                case "Vänster": _position.Item1--;
+                case "Vänster":
+                    _position.Item1--;
                     break;
             }
+
+            Board.ShowMap(this);
         }
 
-        public abstract void PresentPlayer();
+        public virtual void PresentPlayer()
+        {
+            Console.Write($"Din karaktär heter {_name} och är en ");
+        }
 
         public abstract void Attack();
 
@@ -220,12 +318,12 @@
 
         }
 
-        public void Heal() // Make abstract?
+        public void Heal()
         {
 
         }
 
-        public void TakeDamage() // Make abstract?
+        public void TakeDamage()
         {
 
         }
@@ -237,7 +335,8 @@
 
         public override void PresentPlayer()
         {
-            Console.WriteLine($"Din karaktär heter {Name} och är en eldhjälte.");
+            base.PresentPlayer();
+            Console.WriteLine("eldhjälte");
         }
 
         public override void Attack()
@@ -252,7 +351,8 @@
 
         public override void PresentPlayer()
         {
-            Console.WriteLine($"Din karaktär heter {Name} och är en ishjälte.");
+            base.PresentPlayer();
+            Console.WriteLine("ishjälte");
         }
 
         public override void Attack()
@@ -270,12 +370,12 @@
 
         }
 
-        public void Heal() // Make virtual?
+        public void Heal()
         {
 
         }
 
-        public void TakeDamage() // Make virtual?
+        public void TakeDamage()
         {
 
         }
